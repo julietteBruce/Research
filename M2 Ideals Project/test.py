bf8f6efb7c3c1fd7b3dc578e2867bdae9a912390
Juliette Bruce <jemmy.bruce@gmail.com>
2023-03-06 20:26:57 -0500

    Update test.py

--- M2 Ideals Project/test.py
+++ M2 Ideals Project/test.py
@@ -14,18 +14,32 @@
 
 # Idea: Create a M2 package that reads each line 
 
-file_name = "___Virtual__Resolutions.out"
+# file_name = "___Virtual__Resolutions.out"
+file_name = "example-output/_resolve__Via__Fat__Point.out"
 file = open(file_name,'r')
 lines = file.readlines()
-print(lines)
+# print(lines)
+def delimeter_count(line: str) -> int:
+	paren_number = (line.count("(")-line.count(")"))
+	bracket_number = (line.count("[")-line.count("]"))
+	brace_number = (line.count("{")-line.count("}"))
+	return [paren_number,bracket_number,brace_number]
+
+def sum_two_lists(lsit1, list2):
+	return [sum(x) for x in zip(lsit1,list2)]
+
 previous_line = ""
+running_delimeter_count = [0,0,0]
 for line in lines:
 	if line.strip():
 		if line[0] == "i":
 			print(f"{line}")
 			previous_line = "i"
+			running_delimeter_count = sum_two_lists(delimeter_count(line),running_delimeter_count)
 		elif line[0] == "o":
 			previous_line = "o"
 		else:
-			if previous_line == "i":
-				print(f"{previous_line}, {line}")
+			if previous_line == "i" and running_delimeter_count != [0,0,0]:
+				print(f"{line}")
+				#print(f"{previous_line},{running_delimeter_count}, {line}")
+				running_delimeter_count = sum_two_lists(delimeter_count(line),running_delimeter_count)
