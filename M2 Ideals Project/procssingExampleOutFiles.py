8befb3bdc162f4b607f45eb9b756c2641be6339f
Juliette Bruce <jemmy.bruce@gmail.com>
2023-03-07 10:32:29 -0500

    Update procssingExampleOutFiles.py

--- M2 Ideals Project/procssingExampleOutFiles.py
+++ M2 Ideals Project/procssingExampleOutFiles.py
@@ -1,21 +1,3 @@
-# Part 1. Example Gathering and Clean-Up
-
-# Step 1: Generate output files for all Macaulay2 packages
-# Step 2: For each package combine all of the output files into a single output file 
-# Step 3: Clean up each combinded output file by: i) deleting all lines not corresponding to an input
-# (Warning: This might be a pain if the input is broken into multiple lines. ii) delete the input lines
-# symbol.
-
-# /usr/local/Cellar/macaulay2/1.19.1_3
-
-# share/doc/Macaulay2/PACKAGENAME/example-output
-
-# Part 2. Run Examples
-
-# Idea: Create a M2 package that reads each line 
-
-# file_name = "___Virtual__Resolutions.out"
-
 from typing import List, Dict, Literal
 
 def signed_delimiter_count(line: str, delimeter: str) -> int:
@@ -66,19 +48,5 @@ delimiter_pairs = ['()', '[]', "{}"]
 
 process_example_out_file(input_file_name,output_file_name,delimiter_pairs)
 
-# for line in lines:
-# 	if line.strip():
-# 		if line[0] == "i":
-# 			if (line.split(':',1))[1] != ' \n':
-# 				print(f"E{example_number} = {(line.split(':',1))[1]}")
-# 				previous_line = "i"
-# 				running_delimeter_count = update_running_delimeter_count(line,delimiter_pairs,running_delimeter_count)
-# 				example_number += 1
-# 		elif line[0] == "o":
-# 			previous_line = "o"
-# 		else:
-# 			if previous_line == "i" and running_delimeter_count != [0,0,0]:
-# 				print(f"{line}")
-# 				running_delimeter_count = sum_two_lists(count_delimiters(line,delimiter_pairs),running_delimeter_count)
 
 
